95643d04c6cee1b8bbea538e3f04c5aaf4c20a0a
peiqi <xueman@163.com>
2019-06-24 20:48:18 +0800

    noting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './App.jsx'
-import{getStorage,setStorage} from './utils/storageUtils'
+import{ getStorage } from './utils/storageUtils'
 import  memoryUtils from './utils/memoryUtils'
 /* 浏览器一加载就将localStorage中的key以及value写入到user中
  */
